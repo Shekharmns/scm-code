94e8a4e18fcc6b55434c52fbfc3585cb2dfef90a
Shekharmns <129092198+Shekharmns@users.noreply.github.com>
2023-07-30 14:02:01 +0530

    new final

--- 6.py
+++ 6.py
@@ -20,7 +20,8 @@ def lambda_handler(event, context):
     for reservation in ec2_response['Reservations']:
         for instance in reservation['Instances']:
             instance_id = instance['InstanceId']
-            has_cloudwatch_alarm = check_cloudwatch_alarm(cloudwatch_client, 'InstanceId', instance_id)
+            alarm_name = f"EC2_CPU_Utilization_{instance_id}"
+            has_cloudwatch_alarm = check_cloudwatch_alarm(cloudwatch_client, alarm_name)
             ec2_instances.append({
                 'InstanceId': instance_id,
                 'HasCloudWatchAlarm': has_cloudwatch_alarm
@@ -34,7 +35,8 @@ def lambda_handler(event, context):
     rds_response = rds_client.describe_db_instances()
     for instance in rds_response['DBInstances']:
         db_instance_identifier = instance['DBInstanceIdentifier']
-        has_cloudwatch_alarm = check_cloudwatch_alarm(cloudwatch_client, 'DBInstanceIdentifier', db_instance_identifier)
+        alarm_name = f"RDS_Free_Storage_Space_{db_instance_identifier}"
+        has_cloudwatch_alarm = check_cloudwatch_alarm(cloudwatch_client, alarm_name)
         rds_instances.append({
             'DBInstanceIdentifier': db_instance_identifier,
             'HasCloudWatchAlarm': has_cloudwatch_alarm
@@ -49,13 +51,9 @@ def lambda_handler(event, context):
         "RDSInstances": rds_instances
     }
 
-def check_cloudwatch_alarm(client, dimension_name, dimension_value):
-    alarms = client.describe_alarms(AlarmNamePrefix=dimension_value)
-    for alarm in alarms['MetricAlarms']:
-        for dimension in alarm['Dimensions']:
-            if dimension['Name'] == dimension_name and dimension['Value'] == dimension_value:
-                return True
-    return False
+def check_cloudwatch_alarm(client, alarm_name):
+    alarms = client.describe_alarms(AlarmNames=[alarm_name])
+    return len(alarms['MetricAlarms']) > 0
     
 def create_cpu_utilization_alarm(client, instance_id, sns_topic_arn):
     client.put_metric_alarm(
@@ -67,7 +65,7 @@ def create_cpu_utilization_alarm(client, instance_id, sns_topic_arn):
         Period=60,
         Statistic='Average',
         Threshold=70.0,
-        ActionsEnabled=False,
+        ActionsEnabled=True,  # Set to True to enable alarm actions
         AlarmDescription=f"Alarm for EC2 instance {instance_id} CPU utilization exceeding 70%",
         Dimensions=[
             {
@@ -91,7 +89,7 @@ def create_free_storage_space_alarm(client, db_instance_identifier, sns_topic_ar
         Period=60,
         Statistic='Average',
         Threshold=5000000000,  # 5 GB in bytes
-        ActionsEnabled=False,
+        ActionsEnabled=True,  # Set to True to enable alarm actions
         AlarmDescription=f"Alarm for RDS instance {db_instance_identifier} free storage space less than 5 GB",
         Dimensions=[
             {
